092563421071f4327e32bcdb326b52f1b59c4c05
jamilkasan <jamilkasan123@gmail.com>
2023-03-25 09:40:49 -0300

    Initial commit

---
+++ app/Filament/Resources/SessionResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SessionResource\Pages;
+use App\Filament\Resources\SessionResource\RelationManagers;
+use App\Models\Session;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class SessionResource extends Resource
+{
+    protected static ?string $model = Session::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('date'),
+                Tables\Columns\TextColumn::make('duration'),
+                Tables\Columns\TextColumn::make('distance'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSessions::route('/'),
+            'create' => Pages\CreateSession::route('/create'),
+            'edit' => Pages\EditSession::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Models/Session.php
+++ app/Models/Session.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Session extends Model
 {
     use HasFactory;
+
+//    public function User() :BelongsTo
+//    {
+//        return $this->belongsTo(Member::class, 'uid');
+//    }
 }
